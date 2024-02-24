bbe7e8e5bef7c9979655817c42d15d720dee31ff
millyanne93 <nmillyanne20@gmail.com>
2024-02-24 12:25:24 -0800

    fix syntax

--- models/user.py
+++ models/user.py
@@ -28,10 +28,6 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        if 'password' in kwargs:
-            self.password = hashlib.md5(
-                    kwargs['password'].encode()
-            ).hexdigest()
 
     def __setattr__(self, key, value):
         """Override function to hash password attribute"""
