e7cff0bdb7cf0bad125d8c15b2cda9ccd9ef7159
millyanne93 <nmillyanne20@gmail.com>
2024-02-24 11:09:23 -0800

    Add search method

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,8 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.amenity import Amenity
+from models.state import State
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
